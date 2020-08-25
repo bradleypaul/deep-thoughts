ac7c75786f664a2a2786a749551658966ae57a51
Paul Bradley <bradleypauld@gmail.com>
2020-08-25 11:28:39 -0500

    bulk update 21.2.7

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,8 +25,22 @@ const typeDefs = gql`
     createdAt: String
     username: String
   }
+  
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
